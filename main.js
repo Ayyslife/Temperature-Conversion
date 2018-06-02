1db1eb4b72388e45c56cfff33b020b39d0a7736e
AsimAM <asim.a.m@outlook.com>
2018-06-02 13:10:23 +0100

    Created a selector

--- main.js
+++ main.js
@@ -1,7 +1,49 @@
-document.getElementById('output').style.visibility = 'hidden';
-document.getElementById('celsiusInput').addEventListener('input', function(e){
-    document.getElementById('output').style.visibility = 'visible';
-    let celsiusInput = e.target.value;
-    document.getElementById('Fahrenheit-output').innerHTML = celsiusInput * 9/5 + 32;
-    document.getElementById('Kelvin-output').innerHTML = celsiusInput + 273.15;
-})
+document.getElementById("output").style.visibility = "hidden";
+
+let selector = document.getElementById('selector');
+selector.addEventListener('click', function(){
+    if(selector.value == 1){
+        celConvert(); 
+    }
+    else if(selector.value == 2){
+        farConvert();
+    }
+    else{
+        kelConvert();
+    }
+});
+
+function celConvert(){
+    document.getElementById('inputTemp').addEventListener('input', function(e){
+        document.getElementById('output').style.visibility = 'visible';
+        document.getElementById('Fahrenheit-output').style.visibility = 'visible';
+        document.getElementById('Kelvin-output').style.visibility = 'visible';
+        document.getElementById('Celsius-output').style.visibility = 'hidden';
+        let celsiusInput = e.target.value;
+        document.getElementById('Fahrenheit-output').innerHTML = celsiusInput;
+        document.getElementById('Kelvin-output').innerHTML = celsiusInput;
+    });   
+}
+function farConvert(){
+    document.getElementById('inputTemp').addEventListener('input', function(e){
+        document.getElementById('output').style.visibility = 'visible';
+        document.getElementById('Celsius-output').style.visibility = 'visible';
+        document.getElementById('Kelvin-output').style.visibility = 'visible';
+        document.getElementById('Fahrenheit-output').style.visibility = 'hidden';
+        let fahrenheitInput = e.target.value;
+        document.getElementById('Celsius-output').innerHTML = fahrenheitInput;
+        document.getElementById('Kelvin-output').innerHTML = fahrenheitInput;
+    });  
+}
+
+function kelConvert(){
+    document.getElementById('inputTemp').addEventListener('input', function(e){
+        document.getElementById('output').style.visibility = 'visible';
+        document.getElementById('Fahrenheit-output').style.visibility = 'visible';
+        document.getElementById('Celsius-output').style.visibility = 'visible';
+        document.getElementById('Kelvin-output').style.visibility = 'hidden';
+        let kelvinInput = e.target.value;
+        document.getElementById('Celsius-output').innerHTML = kelvinInput;
+        document.getElementById('Fahrenheit-output').innerHTML = kelvinInput;
+    });  
+}
